d1a5065424f1393a36e8521565a605af8c268ddf
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-13 21:39:43 +0100

    cambios en model de Alumno
    cambios en model de Alumno

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/alumno/models/Alumno.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/alumno/models/Alumno.kt
@@ -3,17 +3,17 @@ package org.example.prestamoordenadores.rest.alumno.models
 import org.example.prestamoordenadores.rest.users.models.User
 import java.time.LocalDateTime
 
-class Alumno {
-    var id: Long = 0
-    var guid : String = ""
-    var numeroEstudiante : String = ""
-    var nombre: String = ""
-    var apellidos: String = ""
-    var email: String = ""
-    var curso: String = ""
-    var fotoCarnet: String = ""
-    var user : User = User()
-    var enabled: Boolean = false
-    var createdDate: LocalDateTime = LocalDateTime.now()
+data class Alumno (
+    var id: Long = 0,
+    var guid : String = "",
+    var numeroEstudiante : String = "",
+    var nombre: String = "",
+    var apellidos: String = "",
+    var email: String = "",
+    var curso: String = "",
+    var fotoCarnet: String = "",
+    var user : User = User(),
+    var enabled: Boolean = false,
+    var createdDate: LocalDateTime = LocalDateTime.now(),
     var updatedDate: LocalDateTime = LocalDateTime.now()
-}
+)
