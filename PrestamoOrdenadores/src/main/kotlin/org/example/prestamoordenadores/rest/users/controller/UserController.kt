21550dad4ee530b91d6dd057319f9755ed8eed07
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-14 23:35:18 +0100

    añadidos capturas de error en user controller
    añadidos capturas de error en user controller

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/controller/UserController.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/controller/UserController.kt
@@ -1,13 +1,17 @@
 package org.example.prestamoordenadores.rest.users.controller
 
 import com.github.michaelbull.result.Result
+import com.github.michaelbull.result.mapBoth
 import org.example.prestamoordenadores.rest.users.dto.UserPasswordResetRequest
 import org.example.prestamoordenadores.rest.users.dto.UserRequest
 import org.example.prestamoordenadores.rest.users.dto.UserResponse
 import org.example.prestamoordenadores.rest.users.errors.UserError
+import org.example.prestamoordenadores.rest.users.errors.UserError.UserAlreadyExists
+import org.example.prestamoordenadores.rest.users.errors.UserError.UserNotFound
 import org.example.prestamoordenadores.rest.users.models.User
 import org.example.prestamoordenadores.rest.users.services.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PatchMapping
@@ -27,14 +31,44 @@ class UserController
     fun getAllUsers() : Result<List<User>, UserError> = userService.getAllUsers()
 
     @GetMapping("/{guid}")
-    fun getUserByGuid(@PathVariable guid: String) : Result<UserResponse?, UserError> = userService.getUserByGuid(guid)
+    fun getUserByGuid(@PathVariable guid: String) : ResponseEntity<UserResponse?> {
+        return userService.getUserByGuid(guid).mapBoth(
+            success = { ResponseEntity.ok(it) },
+            failure = { error ->
+                when (error) {
+                    is UserNotFound -> ResponseEntity.notFound().build()
+                    else -> ResponseEntity.status(422).body(null)
+                }
+            }
+        )
+    }
 
     @GetMapping("/{username}")
-    fun getUserByUsername(@PathVariable username: String) : Result<UserResponse?, UserError> = userService.getByUsername(username)
+    fun getUserByUsername(@PathVariable username: String) : ResponseEntity<UserResponse?> {
+        return userService.getByUsername(username).mapBoth(
+            success = { ResponseEntity.ok(it) },
+            failure = { error ->
+                when (error) {
+                    is UserNotFound -> ResponseEntity.notFound().build()
+                    else -> ResponseEntity.status(422).body(null)
+                }
+            }
+        )
+    }
 
     @PostMapping
-    fun createUser(@RequestBody user: UserRequest) : Result<UserResponse, UserError> = userService.createUser(user)
-
+    fun createUser(@RequestBody user: UserRequest): ResponseEntity<UserResponse> {
+        return userService.createUser(user).mapBoth(
+            success = { ResponseEntity.status(201).body(it) },
+            failure = { error ->
+                when (error) {
+                    is UserNotFound -> ResponseEntity.notFound().build()
+                    is UserAlreadyExists -> ResponseEntity.badRequest().build()
+                }
+            }
+        )
+    }
+    
     @PatchMapping("/{guid}")
     fun resetPassword(@PathVariable guid: String, @RequestBody user: UserPasswordResetRequest) : Result<UserResponse?, UserError> = userService.resetPassword(guid, user)
 
