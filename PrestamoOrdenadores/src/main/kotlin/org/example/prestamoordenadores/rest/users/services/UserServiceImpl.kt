d53670b350aa9dd37e80d13c7f6747585d9fb35b
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-14 22:05:48 +0100

    hecho servicio de user
    hecho servicio de user

---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/services/UserServiceImpl.kt
@@ -0,0 +1,75 @@
+package org.example.prestamoordenadores.rest.users.services
+
+import com.github.michaelbull.result.Err
+import com.github.michaelbull.result.Ok
+import com.github.michaelbull.result.Result
+import org.example.prestamoordenadores.rest.users.dto.UserPasswordResetRequest
+import org.example.prestamoordenadores.rest.users.dto.UserRequest
+import org.example.prestamoordenadores.rest.users.dto.UserResponse
+import org.example.prestamoordenadores.rest.users.errors.UserError
+import org.example.prestamoordenadores.rest.users.mappers.UserMapper
+import org.example.prestamoordenadores.rest.users.models.User
+import org.example.prestamoordenadores.rest.users.repositories.UserRepository
+import org.lighthousegames.logging.logging
+import org.springframework.stereotype.Service
+
+private val logger = logging()
+
+@Service
+class UserServiceImpl(
+    private val repository : UserRepository,
+    private val mapper: UserMapper
+): UserService {
+    override fun getAllUsers(): Result<List<User>, UserError> {
+        logger.debug { "Getting all users" }
+        return Ok(repository.findAll())
+    }
+
+    override fun getUserByGuid(guid: String): Result<UserResponse?, UserError> {
+        logger.debug { "Getting user by GUID: $guid" }
+        var user = repository.findByGuid(guid)
+
+        return if (user == null) {
+            Err(UserError.UserNotFound("User with GUID $guid not found"))
+        } else {
+            Ok(mapper.toUserResponse(user))
+        }
+    }
+
+    override fun createUser(user: UserRequest): Result<UserResponse, UserError> {
+        logger.debug { "Creating user: $user" }
+        val existingUser = repository.findByUsername(user.username)
+        if (existingUser!= null) {
+            return Err(UserError.UserAlreadyExists("Username ${user.username} already exists"))
+        }
+        val savedUser = repository.save(mapper.toUserFromCreate(user))
+        return Ok(mapper.toUserResponse(savedUser))
+    }
+
+    override fun deleteUserByGuid(guid: String): Result<UserResponse?, UserError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByUsername(username: String): Result<UserResponse?, UserError> {
+        logger.debug { "Getting user by username: $username" }
+        val user = repository.findByUsername(username)
+        if (user == null) {
+            return Err(UserError.UserNotFound("User with username $username not found"))
+        }
+        return Ok(mapper.toUserResponse(user))
+    }
+
+    override fun resetPassword(
+        guid: String,
+        user: UserPasswordResetRequest
+    ): Result<UserResponse?, UserError> {
+        logger.debug { "Resetting password for user with GUID: $guid" }
+        val existingUser = repository.findByGuid(guid)
+        if (existingUser == null) {
+            return Err(UserError.UserNotFound("User with GUID $guid not found"))
+        }
+        existingUser.password = user.newPassword
+        val savedUser = repository.save(existingUser)
+        return Ok(mapper.toUserResponse(savedUser))
+    }
+}
