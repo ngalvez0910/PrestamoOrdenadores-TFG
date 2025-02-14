29607a845a0608f8487aa2f3f832dcde1b9a06e3
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-14 22:05:41 +0100

    hecho mapper de user
    hecho mapper de user

---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/mappers/UserMapper.kt
@@ -0,0 +1,33 @@
+package org.example.prestamoordenadores.rest.users.mappers
+
+import org.example.prestamoordenadores.rest.users.dto.UserRequest
+import org.example.prestamoordenadores.rest.users.dto.UserResponse
+import org.example.prestamoordenadores.rest.users.models.Role
+import org.example.prestamoordenadores.rest.users.models.User
+import org.springframework.cglib.core.Local
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+
+@Component
+class UserMapper {
+    fun toUserResponse(user: User): UserResponse{
+        return UserResponse(
+            username = user.username,
+            password = user.password,
+            lastPasswordResetDate = user.lastPasswordResetDate.toString()
+        )
+    }
+
+    fun toUserFromCreate(user: UserRequest): User {
+        return User(
+            username = user.username,
+            password = user.password,
+            roles = Role.ALUMNO,
+            enabled = true,
+            createdDate = LocalDateTime.now(),
+            updatedDate = LocalDateTime.now(),
+            lastLoginDate = LocalDateTime.now(),
+            lastPasswordResetDate = LocalDateTime.now()
+        )
+    }
+}
