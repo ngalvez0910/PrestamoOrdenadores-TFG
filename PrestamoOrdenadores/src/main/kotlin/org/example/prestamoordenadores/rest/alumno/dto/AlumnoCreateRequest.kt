a69a7323da033ffd18e28ea102960cb312eeea59
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-14 13:25:58 +0100

    creado request de create
    creado request de create

---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/alumno/dto/AlumnoCreateRequest.kt
@@ -0,0 +1,13 @@
+package org.example.prestamoordenadores.rest.alumno.dto
+
+data class AlumnoCreateRequest (
+    var numeroEstudiante: String,
+    var nombre: String,
+    var apellidos: String,
+    var email: String,
+    var curso: String,
+    var fotoCarnet: String,
+    var username: String,
+    var password: String,
+    var confirmedPassword: String
+)
