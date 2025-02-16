ce91973363325889eba2c30a39a6a9c4ef89952d
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-16 19:10:41 +0100

    actualizacion en panel dispositivos Vue
    actualizacion en panel dispositivos Vue. faltan cambios

--- PrestamoOrdenadores/prestamoOrdenadores-frontend/src/views/DispositivosDashboard.vue
+++ PrestamoOrdenadores/prestamoOrdenadores-frontend/src/views/DispositivosDashboard.vue
@@ -1,5 +1,15 @@
 <template>
   <MenuBar />
+  <p class="nameFilter">
+    Nombre:
+    <input
+        type="text"
+        v-model="search"
+        placeholder="Buscar pokemon..."
+    />
+    &nbsp;
+  </p>
+  <br>
   <div class="table">
     <DataTable stripedRows tableStyle="min-width: 50rem">
       <Column field="id" header="ID"></Column>
@@ -20,11 +30,30 @@ import MenuBar from "@/components/AdminMenuBar.vue";
 export default {
   name: 'DispositivosDashboard',
   components: {MenuBar},
+  data() {
+    return {
+      search: '',
+    }
+  },
 };
 </script>
 
 <style>
+.nameFilter {
+  display: flex;
+  margin-left: -30%;
+  margin-top: -30%;
+  padding: 1rem;
+}
+
+.nameFilter input{
+  width: 20%;
+}
+
 .table {
   display: flex;
+  margin-left: -15%;
+  margin-top: -5%;
+  padding: 1rem;
 }
 </style>
