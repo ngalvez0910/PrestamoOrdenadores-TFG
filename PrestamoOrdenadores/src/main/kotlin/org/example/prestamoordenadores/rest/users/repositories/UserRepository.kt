2d67f30dce91d74103f962f8c51f96dd4d99562d
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-23 16:25:13 +0100

    cambios en repositorio y servicio de user
    cambios en repositorio y servicio de user

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/repositories/UserRepository.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/repositories/UserRepository.kt
@@ -1,13 +1,19 @@
 package org.example.prestamoordenadores.rest.users.repositories
 
-import org.example.prestamoordenadores.rest.users.dto.UserRequest
+import org.example.prestamoordenadores.rest.users.dto.UserCreateRequest
+import org.example.prestamoordenadores.rest.users.dto.UserResponse
 import org.example.prestamoordenadores.rest.users.models.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface UserRepository: JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-    fun findByGuid(guid: String): User?
-    fun save(userRequest: UserRequest): User?
+    fun findByUsername(username: String): UserResponse?
+    fun findByGuid(guid: String): UserResponse?
+    fun save(userRequest: UserCreateRequest): UserResponse?
+    fun findByCurso(curso: String): List<UserResponse?>
+    fun findByNombre(nombre: String): UserResponse?
+    fun findByEmail(email: String): UserResponse?
+    fun findByTutor(tutor: String): UserResponse?
+    fun existsAlumnoByNombreAndApellidosAndCurso(nombre: String, apellidos:String, curso: String): Boolean
 }
