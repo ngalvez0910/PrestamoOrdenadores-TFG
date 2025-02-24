f3879d2c51a9454869b0d7cb056242f77ee39392
Natalia González Álvarez <nagonal2004@gmail.com>
2025-02-24 12:05:58 +0100

    hecha base de servicio de prestamo
    hecha base de servicio de prestamo

---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/prestamos/services/PrestamoService.kt
@@ -0,0 +1,17 @@
+package org.example.prestamoordenadores.rest.prestamos.services
+
+import com.github.michaelbull.result.Result
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+interface PrestamoService {
+    fun getAllPrestamos(): Result<List<PrestamoResponse>, PrestamoError>
+    fun getPrestamoByGuid(guid: String) : Result<PrestamoResponse?, PrestamoError>
+    fun createPrestamo(prestamo: PrestamoCreateRequest) : Result<PrestamoResponse, PrestamoError>
+    fun updatePrestamo(prestamo: PrestamoUpdateRequest) : Result<PrestamoResponse?, PrestamoError>
+    fun deletePrestamoByGuid(guid: String) : Result<PrestamoResponse?, PrestamoError>
+    fun getByFechaPrestamo(fecha: LocalDate): Result<List<PrestamoResponse?>, PrestamoError>
+    fun getByFechaLimite(fecha: LocalDate): Result<List<PrestamoResponse?>, PrestamoError>
+    fun getPrestamoByUserGuid(userGuid: String) : Result<List<PrestamoResponse?>, PrestamoError>
+}
---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/prestamos/services/PrestamoServiceImpl.kt
@@ -0,0 +1,43 @@
+package org.example.prestamoordenadores.rest.prestamos.services
+
+import com.github.michaelbull.result.Result
+import org.example.prestamoordenadores.rest.prestamos.repositories.PrestamoRepository
+import org.springframework.stereotype.Service
+import java.time.LocalDate
+
+@Service
+class PrestamoServiceImpl(
+    private val prestamoRepository: PrestamoRepository
+): PrestamoService {
+    override fun getAllPrestamos(): Result<List<PrestamoResponse>, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPrestamoByGuid(guid: String): Result<PrestamoResponse, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun createPrestamo(prestamo: PrestamoCreateRequest): Result<PrestamoResponse, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updatePrestamo(prestamo: PrestamoUpdateRequest): Result<PrestamoResponse, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun deletePrestamoByGuid(guid: String): Result<PrestamoResponse, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByFechaPrestamo(fecha: LocalDate): Result<List<PrestamoResponse>, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getByFechaLimite(fecha: LocalDate): Result<List<PrestamoResponse>, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getPrestamoByUserGuid(userGuid: String): Result<List<PrestamoResponse>, PrestamoError> {
+        TODO("Not yet implemented")
+    }
+}
