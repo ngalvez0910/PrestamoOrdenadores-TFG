988d103262b63eeb1959cade83b24a5860d2ba9d
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-14 17:07:01 +0100

    añadidos errores
    añadidos errores

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/dispositivos/errors/DispositivoError.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/dispositivos/errors/DispositivoError.kt
@@ -2,4 +2,5 @@ package org.example.prestamoordenadores.rest.dispositivos.errors
 
 sealed class DispositivoError(var message: String) {
     class DispositivoNotFound(message: String) : DispositivoError(message)
+    class DispositivoAlreadyExists(message: String) : DispositivoError(message)
 }
