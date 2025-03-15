93b0a4dc7e20d954110258d7550feeff998afdc8
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-15 17:15:44 +0100

    cambio de anotaciones de model a dto user
    cambio de anotaciones de model a dto user

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/dto/UserCreateRequest.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/dto/UserCreateRequest.kt
@@ -1,13 +1,22 @@
 package org.example.prestamoordenadores.rest.users.dto
 
+import org.jetbrains.annotations.NotNull
+
 data class UserCreateRequest (
+    @NotNull("Numero identificacion no puede ser null")
     var numeroIdentificacion: String,
+    @NotNull("Nombre no puede ser null")
     var nombre: String,
+    @NotNull("Apellidos no puede ser null")
     var apellidos: String,
+    @NotNull("Email no puede ser null")
     var email: String,
     var curso: String,
     var tutor: String,
+    @NotNull("Foto carnet no puede ser null")
     var fotoCarnet: String,
+    @NotNull("Password no puede estar vacío")
     var password : String,
+    @NotNull("Confirm password no puede estar vacío")
     var confirmPassword : String
 )
--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/dto/UserPasswordResetRequest.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/users/dto/UserPasswordResetRequest.kt
@@ -1,7 +1,12 @@
 package org.example.prestamoordenadores.rest.users.dto
 
+import org.jetbrains.annotations.NotNull
+
 data class UserPasswordResetRequest (
+    @NotNull("old password no puede estar vacío")
     var oldPassword: String,
+    @NotNull("new password no puede estar vacío")
     var newPassword: String,
+    @NotNull("confirm password no puede estar vacío")
     var confirmPassword: String
 )
