65403ca091c77bf784749d286db48e8b723d13c4
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-16 13:37:47 +0100

    añadidos valores default a incidencias
    añadidos valores default a incidencias

--- PrestamoOrdenadores/src/main/resources/data.sql
+++ PrestamoOrdenadores/src/main/resources/data.sql
@@ -1,21 +1,7 @@
-/*CREATE OR REPLACE FUNCTION generate_guid() RETURNS VARCHAR(11) AS '
-DECLARE
-    chars TEXT := ''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'';
-    result TEXT := '''';
-    i INT := 0;
-BEGIN
-    WHILE i < 11 LOOP
-        result := result || substring(chars FROM floor(random() * length(chars) + 1)::int FOR 1);
-        i := i + 1;
-    END LOOP;
-    RETURN result;
-END;
-' LANGUAGE plpgsql;
-
 -- Crear tabla usuarios
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
-    guid VARCHAR(11) NOT NULL UNIQUE DEFAULT generate_guid(),
+    guid VARCHAR(11) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     rol VARCHAR(8) NOT NULL,
@@ -33,8 +19,6 @@ CREATE TABLE IF NOT EXISTS usuarios (
     last_password_reset_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
- */
-
 -- Insertar usuarios de prueba
 INSERT INTO usuarios (guid, email, password, rol, numero_identificacion, nombre, apellidos, curso, tutor, foto_carnet, avatar, is_activo, created_date, updated_date, last_login_date, last_password_reset_date)
 VALUES
@@ -42,22 +26,39 @@ VALUES
     ('c1f40bb1900', 'maria@profesor.colegio.com', 'password456', 'PROFESOR', '87654321B', 'María', 'Gómez', '1º Bachillerato', NULL, 'foto2.jpg', 'avatar2.png', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
     ('0d6d031ad0a', 'admin@admin.colegio.com', 'adminpassword', 'ADMIN', '00000000X', 'Admin', 'User', NULL, NULL, 'foto3.jpg', 'avatar3.png', true, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
-/*
+-- Crear tabla incidencias
+CREATE TABLE IF NOT EXISTS incidencias (
+    id SERIAL PRIMARY KEY,
+    guid VARCHAR(11) NOT NULL UNIQUE,
+    asunto VARCHAR(500) NOT NULL,
+    descripcion VARCHAR(999) NOT NULL,
+    estado_incidencia VARCHAR(9) NOT NULL,
+    user_guid VARCHAR(11) NULL DEFAULT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Insertar incidencias de prueba
+INSERT INTO incidencias (guid, asunto, descripcion, estado_incidencia, user_guid, created_date, updated_date)
+VALUES
+    ('INC000003', 'Cargador roto', 'El cargador esta despeluchado', 'PENDIENTE', '3854b5ba26c', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('INC000006', 'Tecla W rota', 'La tecla W esta levantada y no se puede volver a colocar', 'PENDIENTE', 'c1f40bb1900', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
+    ('INC000010', 'Virus', 'El ordenador tiene un virus', 'PENDIENTE', 'c1f40bb1900', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+
 -- Crear tabla dispositivos
 CREATE TABLE IF NOT EXISTS dispositivos (
     id SERIAL PRIMARY KEY,
-    guid VARCHAR(11) NOT NULL UNIQUE DEFAULT generate_guid(),
+    guid VARCHAR(11) NOT NULL UNIQUE,
     numero_serie VARCHAR(255) NOT NULL UNIQUE,
     componentes VARCHAR(255) NOT NULL,
     estado_dispositivo VARCHAR(13) NOT NULL,
-    incidencia_guid VARCHAR(11) NULL DEFAULT NULL,
+    incidencia_guid VARCHAR(9) NULL DEFAULT NULL,
     stock INTEGER NOT NULL,
     is_activo BOOLEAN DEFAULT true,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
- */
 
 
 -- Insertar dispositivos de prueba
