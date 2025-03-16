fbaf01c250e480bf7cead15121015ff7055d0e3b
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-16 15:47:17 +0100

    mapper sancion
    mapper sancion

---
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/sanciones/mappers/SancionMapper.kt
@@ -0,0 +1,22 @@
+package org.example.prestamoordenadores.rest.sanciones.mappers
+
+import org.example.prestamoordenadores.rest.sanciones.dto.SancionResponse
+import org.example.prestamoordenadores.rest.sanciones.models.Sancion
+import org.example.prestamoordenadores.utils.locale.toDefaultDateTimeString
+import org.springframework.stereotype.Component
+
+@Component
+class SancionMapper {
+    fun toSancionResponse(sancion: Sancion) : SancionResponse{
+        return SancionResponse(
+            guid = sancion.guid,
+            userGuid = sancion.userGuid,
+            tipoSancion = sancion.tipoSancion.toString(),
+            createdDate = sancion.createdDate.toDefaultDateTimeString()
+        )
+    }
+
+    fun toSancionResponseList(sanciones: List<Sancion?>): List<SancionResponse> {
+        return sanciones.map { toSancionResponse(it!!) }
+    }
+}
