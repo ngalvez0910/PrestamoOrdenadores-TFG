7e9576ba35857bc8c749f68e36edefb5c1e0d3b5
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-20 14:31:18 +0100

    hecho validador de incidencia
    hecho validador de incidencia

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/incidencias/errors/IncidenciaError.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/rest/incidencias/errors/IncidenciaError.kt
@@ -3,4 +3,5 @@ package org.example.prestamoordenadores.rest.incidencias.errors
 sealed class IncidenciaError(var message: String) {
     class IncidenciaNotFound(message: String) : IncidenciaError(message)
     class UserNotFound(message: String) : IncidenciaError(message)
+    class IncidenciaValidationError(message: String) : IncidenciaError(message)
 }
