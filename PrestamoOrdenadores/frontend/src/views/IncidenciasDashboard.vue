fa634c61aaa41d6eb41c350226386c5efc9aa739
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-20 16:47:46 +0100

    arreglada vista incidenciasDashboard
    arreglada vista incidenciasDashboard

--- PrestamoOrdenadores/frontend/src/views/IncidenciasDashboard.vue
+++ PrestamoOrdenadores/frontend/src/views/IncidenciasDashboard.vue
@@ -1,38 +1,35 @@
 <template>
   <MenuBar />
-  <div class="filters row-12" style="margin-left: -33%;">
+  <div class="filters row-12" style="margin-left: -35%;">
     <div class="filter col-3" style="margin-left: 2%;">
       <label for="username">Usuario:</label>
       <input id="username" type="text" v-model="searchUsername" placeholder="Buscar por usuario..." />
     </div>
-    <div class="filter col-3" style="margin-left: -5%;">
-      <label for="grade">Curso:</label>
-      <input id="grade" type="text" v-model="searchGrade" placeholder="Buscar por curso..." />
+    <div class="filter col-3" style="margin-left: -4%;">
+      <label for="asunto">Asunto:</label>
+      <input id="asunto" type="text" v-model="searchAsunto" placeholder="Buscar por asunto..." />
     </div>
     <p class="filter col-3" style="margin-left: -5%; margin-top: 1%">
       Fecha:
       <input type="date" v-model="searchDate" />
     </p>
     <p class="filter col-3" style="margin-left: -5%; margin-top: 1%">
-      <label for="type">Tipo:</label>
-      <select id="type" type="text" v-model="searchType">
+      <label for="state">Estado:</label>
+      <select id="state" type="text" v-model="searchState">
         <option value="">Todos</option>
-        <option value="Advertencia">Advertencia</option>
-        <option value="Temporal">Bloqueo Temporal</option>
-        <option value="Indefinido">Bloqueo Indefinido</option>
+        <option value="Pendiente">Pendiente</option>
+        <option value="Resuelto">Resuelto</option>
       </select>
     </p>
   </div>
   <br>
   <div class="table row-12" style="margin-left: -25%;">
     <DataTable stripedRows tableStyle="min-width: 50rem">
-      <Column field="number" header="Nº"></Column>
       <Column field="guid" header="GUID"></Column>
-      <Column field="username" header="Nombre de usuario"></Column>
-      <Column field="grade" header="Curso"></Column>
-      <Column field="type" header="Tipo"></Column>
-      <Column field="date" header="Fecha de incidencia"></Column>
+      <Column field="asunto" header="Asunto"></Column>
       <Column field="state" header="Estado"></Column>
+      <Column field="user" header="Usuario"></Column>
+      <Column field="date" header="Fecha de incidencia"></Column>
       <Column field="edit"></Column>
     </DataTable>
   </div>
@@ -47,9 +44,9 @@ export default {
   data() {
     return {
       searchUsername: '',
-      searchGrade: '',
+      searchAsunto: '',
       searchDate: '',
-      searchType: '',
+      searchState: '',
     };
   },
 };
