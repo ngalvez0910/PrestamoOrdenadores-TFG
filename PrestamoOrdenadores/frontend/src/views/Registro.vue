ceb874d960aedf8025933fe912d72a2a0967971c
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-29 12:44:57 +0100

    rediseño de registro
    rediseño de registro

--- PrestamoOrdenadores/frontend/src/views/Registro.vue
+++ PrestamoOrdenadores/frontend/src/views/Registro.vue
@@ -1,91 +1,108 @@
 <template>
+  <MenuBarNoSession />
+
   <div class="register-container">
     <div class="register-box">
       <h1>Registro</h1>
 
-      <label for="name" class="input-label">Nombre</label>
-      <input type="text" class="input-field" name="name" placeholder="Nombre">
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="name" class="input-label"><strong>Nombre</strong></label>
+          <input type="text" class="input-field" name="name" placeholder="Nombre">
+        </div>
 
-      <label for="surname" class="input-label">Apellidos</label>
-      <input type="text" class="input-field" name="surname" placeholder="Apellidos">
+        <div class="col-md-6 mb-3">
+          <label for="surname" class="input-label"><strong>Apellidos</strong></label>
+          <input type="text" class="input-field" name="surname" placeholder="Apellidos">
+        </div>
+      </div>
 
-      <label for="email" class="input-label">Correo electrónico</label>
-      <input class="input-field" type="email" name="email" placeholder="Correo electrónico">
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="grade" class="input-label"><strong>Curso</strong></label>
+          <input class="input-field" type="text" name="grade" placeholder="Curso">
+        </div>
 
-      <label for="password" class="input-label">Contraseña</label>
-      <input type="password" name="password" class="input-field" placeholder="Contraseña">
+        <div class="col-md-6 mb-3">
+          <label for="email" class="input-label"><strong>Correo electrónico</strong></label>
+          <input class="input-field" type="email" name="email" placeholder="Correo electrónico">
+        </div>
+      </div>
 
-      <label for="passwordConfirm" class="input-label">Confirmar contraseña</label>
-      <input type="password" name="passwordConfirm" class="input-field" placeholder="Confirmar contraseña">
+      <div class="row">
+        <label for="image" class="input-label"><strong>Foto carnet de estudiante</strong></label>
+        <input class="input-field" type="file" name="image">
+      </div>
 
-      <label for="grade" class="input-label">Curso</label>
-      <input class="input-field" type="text" name="grade" placeholder="Curso">
+      <div class="row">
+        <div class="col-md-6 mb-3">
+          <label for="password" class="input-label"><strong>Contraseña</strong></label>
+          <input type="password" name="password" class="input-field" placeholder="Contraseña">
+        </div>
 
-      <label for="image" class="input-label">Foto carnet de estudiante</label>
-      <input class="input-field" type="file" name="image">
+        <div class="col-md-6 mb-3">
+          <label for="passwordConfirm" class="input-label"><strong>Confirmar contraseña</strong></label>
+          <input type="password" name="passwordConfirm" class="input-field" placeholder="Confirmar contraseña">
+        </div>
+      </div>
 
       <button label="Register">Registrarse</button>
 
-      <p class="register-link"><a href="/">Login</a></p>
+      <p class="login-link"><a href="/">Login</a></p>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+  import MenuBarNoSession from "@/components/MenuBarNoSession.vue";
+
   export default {
     name: 'Register',
+    components: {MenuBarNoSession},
   };
 </script>
 
 <style>
 body {
-  font-family: 'Arial',sans-serif;
+  font-family: 'Arial', sans-serif;
   background-color: #e6e8f0;
   color: #14124f;
-  margin-left: 28%;
-  height: 100%;
 }
 
 .register-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
   width: 100%;
-  margin-top: 5%;
-  margin-bottom: 5%;
+  min-height: 100vh;
+  padding: 20px 0;
+  margin-top: 45%;
 }
 
 .register-box {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 20px;
   background-color: white;
+  padding: 30px;
   border-radius: 10px;
   box-shadow: 0 8px 16px rgba(20, 18, 79, 0.3);
-  width: 500px;
+  width: 100%;
+  max-width: 500px;
   box-sizing: border-box;
+  margin-top: 25%;
 }
 
-h1{
-  margin: 10px 0;
+h1 {
+  text-align: center;
+  margin-bottom: 20px;
 }
 
 .input-label {
-  font-size: 1rem;
   color: #14124f;
-  font-weight: 500;
-  margin-bottom: 8px;
 }
 
 .input-field {
-  border-radius: 8px;
-  padding: 14px;
+  border-radius: 30px;
+  padding: 8px;
   border: 1px solid #d1d3e2;
-  width: 100%;
-  font-size: 1rem;
-  margin-bottom: 18px;
   transition: border 0.3s ease;
   outline: none;
 }
@@ -98,26 +115,27 @@ button {
   background-color: #d6621e;
   color: white;
   border: none;
-  border-radius: 8px;
-  padding: 14px;
+  border-radius: 30px;
+  padding: 5px;
   font-size: 1.1rem;
-  width: 100%;
-  cursor: pointer;
+  width: 50%;
   transition: background-color 0.3s ease;
-  margin-bottom: 20px;
-  margin-top: 18px;
+  margin-bottom: 10px;
+  margin-left: 50%;
 }
 
 button:hover {
   background-color: #a14916;
 }
 
-.register-link a {
+.login-link a {
   color: #14124f;
   text-decoration: none;
+  justify-content: center;
+  display: flex;
 }
 
-.register-link a:hover {
+.login-link a:hover {
   background-color: inherit !important;
 }
 </style>
