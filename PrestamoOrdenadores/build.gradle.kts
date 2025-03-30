f51e809c3b823bb24fbd8358fe84cc945c254b0f
Natalia González Álvarez <nagonal2004@gmail.com>
2025-03-30 11:46:25 +0200

    importadas validaciones
    importadas validaciones

--- PrestamoOrdenadores/build.gradle.kts
+++ PrestamoOrdenadores/build.gradle.kts
@@ -62,6 +62,9 @@ dependencies {
     // Logger
     implementation("org.lighthousegames:logging:1.3.0")
     implementation("ch.qos.logback:logback-classic:1.4.14")
+
+    // Validaciones
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 }
 
 kotlin {
