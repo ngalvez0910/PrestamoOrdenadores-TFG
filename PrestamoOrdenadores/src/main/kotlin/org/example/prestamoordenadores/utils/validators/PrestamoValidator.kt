ade7e3745de2edeb5a90e625e499431425a3e98c
Natalia González Álvarez <nagonal2004@gmail.com>
2025-04-16 13:19:34 +0200

    PrestamoValidatorTest
    PrestamoValidatorTest

--- PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/utils/validators/PrestamoValidator.kt
+++ PrestamoOrdenadores/src/main/kotlin/org/example/prestamoordenadores/utils/validators/PrestamoValidator.kt
@@ -5,9 +5,10 @@ import com.github.michaelbull.result.Ok
 import com.github.michaelbull.result.Result
 import org.example.prestamoordenadores.rest.prestamos.dto.PrestamoUpdateRequest
 import org.example.prestamoordenadores.rest.prestamos.errors.PrestamoError
+import org.example.prestamoordenadores.rest.prestamos.models.EstadoPrestamo
 
 fun PrestamoUpdateRequest.validate(): Result<PrestamoUpdateRequest, PrestamoError> {
-    if (this.estado.isBlank()) {
+    if (this.estado.isBlank() || this.estado.uppercase() !in EstadoPrestamo.entries.map { it.name }) {
         return Err(PrestamoError.PrestamoValidationError("El estado del préstamo no puede estar vacío"))
     }
 
