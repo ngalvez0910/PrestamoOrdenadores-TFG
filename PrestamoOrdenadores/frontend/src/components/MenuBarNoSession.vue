2ace0c35417f90618f168ee1cfaae0f615662f1e
Natalia González Álvarez <nagonal2004@gmail.com>
2025-05-04 14:55:14 +0200

    cambio de estilo en menubar
    cambio de estilo en menubar

--- PrestamoOrdenadores/frontend/src/components/MenuBarNoSession.vue
+++ PrestamoOrdenadores/frontend/src/components/MenuBarNoSession.vue
@@ -13,6 +13,12 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=Inconsolata:wght@200..900&family=Montserrat:ital,wght@0,100..900;1,100..900&family=NTR&family=Quicksand&display=swap');
+
+body{
+  font-family: 'Montserrat', sans-serif;
+}
+
 .menubar {
   position: fixed;
   top: 0;
@@ -31,7 +37,7 @@ export default {
 .menubar h1 a {
   color: white;
   text-decoration: none;
-  font-size: 1.5rem;
+  font-size: 1.8rem;
   font-weight: bold;
   transition: color 0.3s ease;
 }
