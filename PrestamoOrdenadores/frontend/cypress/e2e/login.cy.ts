dd32923fba55cddfc46484602ee59e529e1fb3aa
Natalia González Álvarez <nagonal2004@gmail.com>
2025-05-18 16:48:01 +0200

    hechos tests de login de cypress
    hechos tests de login de cypress

--- PrestamoOrdenadores/frontend/cypress/e2e/login.cy.ts
+++ PrestamoOrdenadores/frontend/cypress/e2e/login.cy.ts
@@ -1,8 +1,55 @@
-// https://on.cypress.io/api
-
-describe('My First Test', () => {
-  it('visits the app root url', () => {
+describe('Login Page', () => {
+  beforeEach(() => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+  })
+
+  it('muestra el formulario con campos y botón', () => {
+    cy.get('h2').should('contain', 'Iniciar Sesión')
+    cy.get('input#email').should('exist')
+    cy.get('input#password').should('exist')
+    cy.get('button[type="submit"]').should('contain', 'Entrar')
+  })
+
+  it('valida email inválido y muestra error', () => {
+    cy.get('input#email').type('email-invalido')
+    cy.get('input#password').type('password123')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('#email-error').should('contain', 'Correo electrónico inválido')
+    cy.get('.p-toast-message').should('contain', 'Por favor, corrija los errores indicados')
+  })
+
+  it('valida password corta y muestra error', () => {
+    cy.get('input#email').type('user@example.com')
+    cy.get('input#password').type('123')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('#password-error').should('contain', 'Contraseña incorrecta')
+    cy.get('.p-toast-message').should('contain', 'Por favor, corrija los errores indicados')
+  })
+
+  it('puede alternar visibilidad de la contraseña', () => {
+    cy.get('input#password').should('have.attr', 'type', 'password')
+    cy.get('.password-toggle-icon').click()
+    cy.get('input#password').should('have.attr', 'type', 'text')
+    cy.get('.password-toggle-icon').click()
+    cy.get('input#password').should('have.attr', 'type', 'password')
+  })
+
+  it('realiza login exitoso y redirige según rol ADMIN', () => {
+    cy.get('#email').type('admin@admin.loantech.com')
+    cy.get('#password').type('Password123?')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/admin/dashboard')
+  })
+
+
+  it('realiza login exitoso y redirige a perfil para usuario normal', () => {
+    cy.get('input#email').type('sofia.rodriguez@loantech.com')
+    cy.get('input#password').type('Password123?')
+    cy.get('button[type="submit"]').click()
+
+    cy.url().should('include', '/profile')
   })
 })
