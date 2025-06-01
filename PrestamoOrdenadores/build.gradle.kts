dd3215fc5c46e7fff30cb6c016b0394747e631b8
Natalia González Álvarez <nagonal2004@gmail.com>
2025-06-01 14:44:34 +0200

    documentado config-auth
    documentado config-auth

--- PrestamoOrdenadores/build.gradle.kts
+++ PrestamoOrdenadores/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.1"
     id("io.spring.dependency-management") version "1.1.7"
+    id("org.jetbrains.dokka") version "2.0.0"
 }
 
 group = "org.example"
